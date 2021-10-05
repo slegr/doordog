2187d5103717a9283120fc769a3432aef57d0d0f
Simon Legros <simonlegros5@gmail.com>
2021-10-05 14:50:49 -0400

    cleanup

--- doordog/app.py
+++ doordog/app.py
@@ -26,27 +26,3 @@ class DoorDog:
         self.app.MainLoop()
         self.device_manager.stop()
         self.device_manager.join()
-
-    # #---------------------------------------------------------------------
-    # def jprint(self, obj):
-    #     # create a formatted string of the Python JSON object
-    #     text = json.dumps(obj, sort_keys=True, indent=4)
-    #     print(text)
-
-    # #---------------------------------------------------------------------
-    # def on_read(self, evt):
-    #     parameters = {
-    #         "reader_id": evt.reader,
-    #         "tag_id": evt.uid
-    #     }
-    #     print("Reading: ", parameters)
-    #     self.frames[evt.reader].setText(evt.uid)
-    #     # response = requests.post("http://raspberrypi/api/scan/", data=parameters, timeout=3)
-    #     response = requests.post("https://lanets.ca/health", timeout=3)
-    #     # Notify.Notification.new("Hi").show()
-    #     if response.status_code == 200 or response.status_code == 201:
-    #         self.jprint(response.json())
-    #     elif response.status_code == 404:
-    #         print("Unknown tag or reader.")
-    #     else:
-    #         print(response)
