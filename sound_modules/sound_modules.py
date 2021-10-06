2c8de88f15fce4483646380627e9eb8f251ddf96
Simon Legros <simonlegros5@gmail.com>
2021-10-06 11:38:36 -0400

    Create a sound test for RPI

---
+++ sound_modules/sound_modules.py
@@ -0,0 +1,79 @@
+from playsound import playsound
+import wx.adv
+from pygame import mixer
+from pydub import AudioSegment
+from pydub.playback import play
+import os
+from time import sleep
+from audioplayer import AudioPlayer
+import simpleaudio as sa
+import pyaudio
+import wave
+
+soundpath = "good_1.wav"
+
+def test_playsound():
+    playsound(soundpath)
+
+def test_pydub():
+    song = AudioSegment.from_wav(soundpath)
+    play(song)
+
+def test_mpg123():
+    os.system("mpg123 " + soundpath)
+
+def test_audioplayer():
+    AudioPlayer(soundpath).play(block=True)
+
+def test_simpleaudio():
+    wave_obj = sa.WaveObject.from_wave_file(soundpath)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+def test_pyaudio():
+    # Set chunk size of 1024 samples per data frame
+    chunk = 1024  
+    # Open the sound file 
+    wf = wave.open(soundpath, 'rb')
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
+    # Open a .Stream object to write the WAV file to
+    # 'output = True' indicates that the sound will be played rather than recorded
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
+    # Read data in chunks
+    data = wf.readframes(chunk)
+    # Play the sound by writing the audio data to the stream
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+    # Close and terminate the stream
+    stream.close()
+    p.terminate()
+
+def test_pygame():
+    # Starting the mixer
+    mixer.init()
+    # Loading the song
+    mixer.music.load(soundpath)
+    # Setting the volume
+    mixer.music.set_volume(1)
+    # Start playing the song
+    mixer.music.play()
+
+def test_func(func):
+    print("Test: " + func.__name__)
+    func()
+    sleep(1)
+
+
+sleep(1)
+test_func(test_playsound)
+test_func(test_pydub)
+test_func(test_mpg123)
+test_func(test_audioplayer)
+test_func(test_simpleaudio)
+test_func(test_pygame)
+test_func(test_pyaudio)
---
+++ sound_modules/start.sh
@@ -0,0 +1,18 @@
+
+sudo apt-get install -y sox
+sudo apt-get install -y libsox-fmt-mp3
+# For mpg123
+sudo apt-get install -y mpg123
+
+# For audioplayer
+sudo apt-get install -y python-gst-1.0 gir1.2-gstreamer-1.0 gstreamer1.0-tools gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly  
+
+# pyaudio
+sudo apt-get install -y python3-pyaudio
+
+pip3 install pygame
+pip3 install pydub
+pip3 install audioplayer
+pip3 install simpleaudio
+
+python3 sound_modules.py
